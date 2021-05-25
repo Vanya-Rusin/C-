ce5bbac602c3d89701e813ad2d88bbc0ef783906
Vanya <74555104+Vanya-Rusin@users.noreply.github.com>
2021-05-26 01:40:37 +0300

    Add files via upload

---
+++ Lab 11/Lab 13/DBSimulation/PC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class PC
+    {
+        
+        public string mail { get; set; }
+        public string countFaculty { get; set; }
+
+        public double lvlAccreditation;
+        public double rating
+        {
+            get
+            {
+                return rating;
+            }
+            set
+            {
+                if (value >= 0)
+                    rating = value;
+                else
+                    throw new Exception("Рівень акредитації не може бути відємним");
+            }
+        }
+
+  
+
+      
+
+        public PC(string mail, string countFaculty, double lvlAccreditation, double rating)
+        {
+            this.mail = mail;
+            this.countFaculty = countFaculty;
+            this.lvlAccreditation = lvlAccreditation;
+            this.rating = rating;
+
+        }
+
+    }
+}
